79913c3f02c0d90f33f5b8a3ae5865e00b82f2aa
KNXCSdev <michal.miksho@gmail.com>
2025-05-26 01:46:49 +0200

    Login page

--- client/src/components/Navigation.tsx
+++ client/src/components/Navigation.tsx
@@ -1,4 +1,9 @@
+import { useLocation } from "react-router";
+
 export default function Navigation() {
+  const location = useLocation();
+  const isLoginPage = location.pathname === "/login";
+
   return (
     <nav className="bg-backgroundPrimary flex items-center justify-between px-16 py-3 shadow-sm">
       <div className="flex items-center gap-2 text-4xl font-semibold">
@@ -10,46 +15,50 @@ export default function Navigation() {
           />
         </a>
       </div>
-      <ul className="flex space-x-12">
-        <li>
-          <a
-            href="/features"
-            className="hover:text-textSecondary text-textAccent text-lg transition-all hover:underline"
-          >
-            Features
-          </a>
-        </li>
-        <li>
-          <a
-            href="/pricing"
-            className="hover:text-textSecondary text-textAccent text-lg transition-all hover:underline"
-          >
-            Pricing
-          </a>
-        </li>
-        <li>
-          <a
-            href="/support"
-            className="hover:text-textSecondary text-textAccent text-lg hover:underline"
-          >
-            Support
-          </a>
-        </li>
-      </ul>
-      <div className="flex items-center space-x-4">
-        <a
-          href="/register"
-          className="bg-textSecondary rounded-lg border border-none px-6 py-2 font-medium text-white transition-all hover:cursor-pointer hover:bg-blue-700"
-        >
-          Get Started
-        </a>
-        <a
-          href="/login"
-          className="bg-backgroundIcon text-textAccent rounded-lg border border-none px-6 py-2 font-semibold hover:cursor-pointer"
-        >
-          Login
-        </a>
-      </div>
+      {!isLoginPage ? (
+        <>
+          <ul className="flex space-x-12">
+            <li>
+              <a
+                href="/features"
+                className="hover:text-textSecondary text-textAccent text-md transition-all hover:underline"
+              >
+                Features
+              </a>
+            </li>
+            <li>
+              <a
+                href="/pricing"
+                className="hover:text-textSecondary text-textAccent text-md transition-all hover:underline"
+              >
+                Pricing
+              </a>
+            </li>
+            <li>
+              <a
+                href="/support"
+                className="hover:text-textSecondary text-textAccent text-md hover:underline"
+              >
+                Support
+              </a>
+            </li>
+          </ul>
+          <div className="flex items-center space-x-4">
+            <a
+              href="/register"
+              className="bg-textSecondary rounded-lg border border-none px-6 py-2 font-medium text-white transition-all hover:cursor-pointer hover:bg-blue-700"
+            >
+              Get Started
+            </a>
+            <a
+              href="/login"
+              className="bg-backgroundIcon text-textAccent rounded-lg border border-gray-300 px-6 py-2 font-semibold hover:cursor-pointer"
+            >
+              Login
+            </a>
+          </div>
+        </>
+      ) : null}
     </nav>
   );
 }
